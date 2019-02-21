b28154ca1682135ff2125c2a7ec350e885d3db7a
zhaohuiliang <stanleyliang5@gmail.com>
2019-02-21 03:07:57 -0500

    initial commit

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,13 +1,13 @@
-def load_urls_from_file(file_path:str):
-    #TODO: add the code needed to read the text file with the urls in it
-    pass
-
-def load_page(url:str):
-    #TODO: add the code to read url
-    pass
-
-def scrape_page(page_contents:str):
-    #TODO: analyze the text
+def load_urls_from_file(file_path: str):
+    # TODO: add the code needed to read the text file with the urls in it
     pass
 
 
+def load_page(url: str):
+    # TODO: add the code to read url
+    pass
+
+
+def scrape_page(page_contents: str):
+    # TODO: analyze the text
+    pass
